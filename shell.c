644282de1249eaebff1566573894e31a8ae5b99b
Bakhtiyar Syed <syed.b@research.iiit.ac.in>
2016-09-28 19:04:55 +0530

    Added signal handling

--- shell.c
+++ shell.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
 #include <string.h>
 #include <sys/utsname.h>
 #include <string.h>
@@ -15,6 +16,7 @@
 
 
 
+int no=0;
 
 
 /**
@@ -26,78 +28,12 @@
 int main(int argc, char **argv)
 {
 
+  
+  //Signal handler for Ctrl-C
+  signal(SIGINT,signalhandler);
 
-  //struct utsname unameData;
-
-  //Returns passwd struct
-  struct passwd * myname=NULL;
-  myname = getpwuid(getuid());
-
-  //Get the hostname
-  char hostname[30];
-  gethostname(hostname, 29);
-
-  //passwd = getpwuid ( getuid());
-  int status;
-  char **args;
-  char *line;
-
-  //Getting the current working dir
-  char * home;
-  char * homenext;
-  char prespath[1000];
-  home = getcwd(curpath, sizeof(curpath));
-  int len = strlen(curpath);
-//printf("len is %d\n",len );
-//printf("curpat is %s\n",curpath );
-  do {
-
-    //Print type
-    printf("<");
-    printf("%s", myname->pw_name);
-    printf("@");
-    printf("%s", hostname);
-    printf(":");
-    homenext= getcwd(prespath, sizeof(prespath));
-    //printf("prespath is %s\n", prespath);
-    if(strlen(prespath)<len)
-        printf("%s",prespath );
-
-    else if (strncmp(curpath,prespath,strlen(prespath))!=0)
-      {
-        if(strlen(prespath)>len)
-        {
-          printf("~");
-          printf ("%s", prespath+ len);
-        }
-        else
-        {
-          printf("%s",prespath );
-        }
-      }
-    else
-      printf("~");
-    printf(">");
-
-    lsh_read_line(); //Calling th function to read the line
-
-    int j=0;
-    while(sentences[j]!=NULL)
-    {
-      args = lsh_split_line(sentences[j]); // Split line
-      status = lsh_execute(args); //Execute the commands
-      j++;
-    }
-
-    //free(line);
-    free(args);
-  } while (status);
-
-
-
-
-
-
+//Starts the shell
+  start(argv);
 
   return EXIT_SUCCESS;
 }
